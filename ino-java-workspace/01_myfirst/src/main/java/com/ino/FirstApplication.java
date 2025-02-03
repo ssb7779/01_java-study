80275d6d1d7b2464f13f82dff57c15680456c372
inhomun <inho8063@naver.com>
2025-02-03 16:33:37 +0900

    add print

--- ino-java-workspace/01_myfirst/src/main/java/com/ino/FirstApplication.java
+++ ino-java-workspace/01_myfirst/src/main/java/com/ino/FirstApplication.java
@@ -1,5 +1,14 @@
 package com.ino;
 
 public class FirstApplication {
-    
+    public void def() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        abc();
+    }
+    public void abc() {
+        def();
+        System.out.println(2);
+    }
 }
