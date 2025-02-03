1aa6acf9650be3fa9b7455ffb53c33ab32b0e33e
kimsh0313 <swe6000@naver.com>
2025-02-03 17:25:26 +0900

    Create 01,02

---
+++ seongho-java-workspace/01_myfirst/src/main/java/com/ksh/FirstApplication.java
@@ -0,0 +1,14 @@
+package com.ksh;
+
+public class FirstApplication {
+    public void abc() {
+
+    }
+
+    public void def() {
+
+    }
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
