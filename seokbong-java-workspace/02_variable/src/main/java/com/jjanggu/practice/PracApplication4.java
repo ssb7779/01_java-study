444a04e8022f9e19da370ae3ebefaad0f3a99d4f
seokbong <ssb7779@naver.com>
2025-02-04 17:45:05 +0900

    02_variable_practice

---
+++ seokbong-java-workspace/02_variable/src/main/java/com/jjanggu/practice/PracApplication4.java
@@ -0,0 +1,13 @@
+package com.jjanggu.practice;
+
+public class PracApplication4 {
+    public static void main(String[] args){
+
+        char ch = 'a';
+//        System.out.println("문자" + ch + "의 unicode : " + (ch+0));
+//        System.out.println("문자" + ch + "의 unicode : " + (int)ch);
+        System.out.printf("문자 %c의 unicode : ", ch);
+        System.out.println((int)ch);
+
+    }
+}
