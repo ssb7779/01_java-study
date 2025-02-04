30865be8a49645f238c0f28168cb0fde993574a6
sotogito <sotogitoarchive@gmail.com>
2025-02-04 17:44:41 +0900

    연산자 생성

---
+++ sooyeon-java-workspace/03_operator/src/main/java/com/sotogito/section01/arithmetic/Application1.java
@@ -0,0 +1,21 @@
+package com.sotogito.section01.arithmetic;
+
+public class Application1 {
+    public static void main(String[] args) {
+        /**
+         * ## 산술연산자
+         * 1. 이항 연산자
+         * 2. 종류 : + - * / %
+         * 3. 우선순위 : (*,%) -> (+,-)
+         */
+
+        int num1 = 10;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
