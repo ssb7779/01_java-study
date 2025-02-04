ec6fb0e144aa50c8fb7a69e79422b030a72ff0a7
이승주 <eun07213@gmail.com>
2025-02-04 17:45:22 +0900

    update pratice

---
+++ seungjoo-java-workspace/02_variable/src/main/java/com/seungjoo/practice/PracApplication4.java
@@ -0,0 +1,8 @@
+package com.seungjoo.practice;
+
+public class PracApplication4 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println("문자 " + ch + "의 unicode : " + (int) ch);
+    }
+}
