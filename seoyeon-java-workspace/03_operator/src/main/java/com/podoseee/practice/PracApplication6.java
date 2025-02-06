631022258a13c0798f227d9d6008125e8744385d
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-02-06 17:31:42 +0900

    update2 03_operator

--- seoyeon-java-workspace/03_operator/src/main/java/com/podoseee/practice/PracApplication6.java
+++ seoyeon-java-workspace/03_operator/src/main/java/com/podoseee/practice/PracApplication6.java
@@ -6,9 +6,11 @@ public class PracApplication6 {
         int math = 80;
         int english = 40;
         int sum = korean + math + english;
-        int average = (korean + math + english) / 3;
+        double average = sum / 3.0; //소수점 표현이 어려워서 데이터 손실이 발생한다. 따라서 3.0을 나눈 것임
+        //int average = (korean + math + english) / 3;
 
-        String result = ((korean>=40)&&(math>=40)&&(english>=40) && average >= 60) ? "합격" : "불합격";
-        System.out.println(result);
+        System.out.println("합계 : " + sum);
+        System.out.println("평균 : " + average);
+        System.out.println((korean>=40)&&(math>=40)&&(english>=40) && average >= 60 ? "합격" : "불합격");
     }
 }
