493ea06dec010c33e295c9a8312a06c1f13c1a55
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-07 17:48:18 +0900

    day5 05_control

---
+++ kyungbae-java-workspace/05_control/src/main/java/com/kyungbae/practice/run/Application.java
@@ -0,0 +1,21 @@
+package com.kyungbae.practice.run;
+
+import com.kyungbae.practice.controller.*;
+
+public class Application {
+
+    public static void main(String[] agrs) {
+
+        ConditionPractice cp = new ConditionPractice();
+
+//        cp.practice1();
+//        cp.practice2();
+//        cp.practice3();
+//        cp.practice4();
+//        cp.practice5();
+        cp.practice6();
+
+    } // main end
+
+
+} // class end
