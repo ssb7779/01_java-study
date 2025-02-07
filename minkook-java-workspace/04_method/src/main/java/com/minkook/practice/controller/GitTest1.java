578f182068d47c5e54da5135657dead510460ac6
jominkook <whaksenaos@naver.com>
2025-02-07 10:13:19 +0900

    1

--- minkook-java-workspace/04_method/src/main/java/com/minkook/practice/controller/GitTest1.java
+++
@@ -1,7 +0,0 @@
-package com.minkook.practice.controller;
-
-public class GitTest1 {
-    public static void main(String[] args) {
-        System.out.println("Commit1테스트");
-    }
-}
--- minkook-java-workspace/04_method/src/main/java/com/minkook/practice/controller/GitTest2.java
+++
@@ -1,7 +0,0 @@
-package com.minkook.practice.controller;
-
-public class GitTest2 {
-    public static void main(String[] args) {
-        System.out.println("Commit2테스트");
-    }
-}
