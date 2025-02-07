3f7aaa2447f66c11ba39a02859a894378056ea18
sotogito <sotogitoarchive@gmail.com>
2025-02-07 17:57:14 +0900

    조건문 업데이트

---
+++ sooyeon-java-workspace/05_control/src/main/java/com/sotogito/section01/control/run/Application.java
@@ -0,0 +1,11 @@
+package com.sotogito.section01.control.run;
+
+import com.sotogito.section01.control.controller.If;
+
+public class Application {
+    public static void main(String[] args) {
+        If i = new If();
+        i.testIFExamole1();
+        
+    }
+}
