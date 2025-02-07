e6772986ee20e304d001ad8ab7fcf379fe7cc7b7
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-07 09:43:21 +0900

    feature : 기능 제목 3

--- inyong-java-workspace/04_method/src/main/java/com/inyong/practice/controller/GitTest2.java
+++ inyong-java-workspace/04_method/src/main/java/com/inyong/practice/controller/GitTest2.java
@@ -2,6 +2,6 @@ package com.inyong.practice.controller;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("commit test 2");
+        System.out.println("commit test 2 로컬에서 수정");
     }
 }
