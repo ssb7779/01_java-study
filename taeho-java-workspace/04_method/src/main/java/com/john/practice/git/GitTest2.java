8349c6fc92e560c6834d68cd4b940b19cd6c0cfe
JohnTH0 <59407190+JohnTH0@users.noreply.github.com>
2025-02-07 09:50:45 +0900

    [Modify] Origin edit
    - 원격에서 파일 변경진행

--- taeho-java-workspace/04_method/src/main/java/com/john/practice/git/GitTest2.java
+++ taeho-java-workspace/04_method/src/main/java/com/john/practice/git/GitTest2.java
@@ -3,5 +3,6 @@ package com.john.practice.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Git Commit Test");
+        System.out.println("원격에서 변경하였습니다");
     }
 }
