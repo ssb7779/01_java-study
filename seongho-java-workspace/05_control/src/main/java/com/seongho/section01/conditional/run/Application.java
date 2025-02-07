7ba319a7538252ede6633199ddc7be65ac680a0d
kimsh0313 <swe6000@naver.com>
2025-02-07 17:50:28 +0900

    05

---
+++ seongho-java-workspace/05_control/src/main/java/com/seongho/section01/conditional/run/Application.java
@@ -0,0 +1,33 @@
+package com.seongho.section01.conditional.run;
+
+import com.seongho.section01.conditional.controller.ConditionPractice;
+import com.seongho.section01.conditional.controller.If;
+import com.seongho.section01.conditional.controller.Switch;
+
+public class Application {
+    public static void main(String[] args){
+        /*
+        ##제어문
+        프로그램 흐름 변경할 때
+        1)조건문 :선택적으로 구문 실행
+        2)반복문: 반복적으로 구문 실행
+
+
+        ##조건문
+        특정 조건에 따라 각기 다른 문장이 수행될 수 있도록 제어
+        조건식의 결과는 반드시 논리값 (t/f)
+
+        종류
+        if =
+            ㄴelse, else if
+        switch =
+         */
+        //If i = new If();
+        //i.testIf();
+        //i.testEIf();
+        //i.testEIf2();
+        //i.testNIfEx2();
+        ConditionPractice S = new ConditionPractice();
+        S.practice4();
+    }
+}
