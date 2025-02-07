92cf0004af3a0092e321ecf4d29533f350715de7
dev-m-ino <inho8063@naver.com>
2025-02-07 09:37:19 +0900

    Update GitTest2.java

--- ino-java-workspace/04_method/src/main/java/com/ino/practice/controller/GitTest2.java
+++ ino-java-workspace/04_method/src/main/java/com/ino/practice/controller/GitTest2.java
@@ -2,6 +2,6 @@ package com.ino.practice.controller;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("Commit2 test");
+        System.out.println("Commit2 test - 원격지에서 수정");
     }
 }
