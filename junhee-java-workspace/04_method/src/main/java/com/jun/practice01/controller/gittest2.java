5fbd737036a1c3a346d979d9f600d7358325071e
hyunjunhee <hojj4714@gmail.com>
2025-02-07 09:37:07 +0900

    Update gittest2.java

--- junhee-java-workspace/04_method/src/main/java/com/jun/practice01/controller/gittest2.java
+++ junhee-java-workspace/04_method/src/main/java/com/jun/practice01/controller/gittest2.java
@@ -2,6 +2,6 @@ package com.jun.practice01.controller;
 
 public class gittest2 {
     public static void main(String[] args) {
-        System.out.println("commit2테스트");
+        System.out.println("commit2테스트-원격지에서수정");
     }
 }
