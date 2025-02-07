ee403cbbd253b45083a0c43d77699525ee113087
jominkook <whaksenaos@naver.com>
2025-02-07 10:04:35 +0900

    feat : 기능제목 수정

--- minkook-java-workspace/04_method/src/main/java/com/minkook/practice/controller/GitTest1.java
+++ minkook-java-workspace/04_method/src/main/java/com/minkook/practice/controller/GitTest1.java
@@ -1,6 +1,6 @@
 package com.minkook.practice.controller;
 
-public class GitTest {
+public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Commit1테스트");
     }
