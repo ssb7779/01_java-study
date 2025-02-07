f249ae784addb1de069b92d3615f2abb0dcf22dc
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-07 09:37:35 +0900

    Update GitTest2.java

--- kyungbae-java-workspace/04_method/src/main/java/com/kyungbae/practice/controller/GitTest2.java
+++ kyungbae-java-workspace/04_method/src/main/java/com/kyungbae/practice/controller/GitTest2.java
@@ -3,7 +3,7 @@ package com.kyungbae.practice.controller;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println(
-                "Commit2테스트"
+                "Commit2테스트-원격지에서수정"
         );
     } // main end
 } // class end
