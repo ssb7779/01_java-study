745ee94ffa3aace08c53c6eeb7bae37f2c9409d6
inhomun <inho8063@naver.com>
2025-02-07 17:02:31 +0900

    Add_250207

---
+++ ino-java-workspace/05_control/src/main/java/com/ino/practice/controller/Application.java
@@ -0,0 +1,8 @@
+package com.ino.practice.controller;
+
+public class Application {
+    public static void main(String[] args) {
+        ConditionPractice cP = new ConditionPractice();
+        cP.menuView();
+    }
+}
