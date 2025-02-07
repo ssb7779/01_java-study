b4a3796bb7a0f767908eb1b944d391835c107249
TH <ok821000@gmail.com>
2025-02-07 09:52:05 +0900

    [Modify] Local File modified - local 환경에서 변경 후 커밋

--- taeho-java-workspace/04_method/src/main/java/com/john/practice/git/GitTest2.java
+++ taeho-java-workspace/04_method/src/main/java/com/john/practice/git/GitTest2.java
@@ -1,7 +1,10 @@
 package com.john.practice.git;
 
+import java.sql.SQLOutput;
+
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Git Commit Test");
+        System.out.println("로컬에서 변경하였습니다");
     }
 }
