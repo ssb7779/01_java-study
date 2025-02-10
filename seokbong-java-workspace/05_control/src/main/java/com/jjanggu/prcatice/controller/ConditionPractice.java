90dee8fe60c861b197144d982a3ca3856b0078c6
seokbong <ssb7779@naver.com>
2025-02-10 19:25:29 +0900

    Prac 05 commit

--- seokbong-java-workspace/05_control/src/main/java/com/jjanggu/prcatice/controller/ConditionPractice.java
+++ seokbong-java-workspace/05_control/src/main/java/com/jjanggu/prcatice/controller/ConditionPractice.java
@@ -1,4 +1,4 @@
-package com.jjanggu.secton01.conditional.controller;
+package com.jjanggu.prcatice.controller;
 import java.util.Scanner;
 
 public class ConditionPractice {
@@ -161,6 +161,65 @@ public class ConditionPractice {
 
 
     public void practice7() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("키(m)를 입력해 주세요. : ");
+        double height = sc.nextDouble();
+
+        System.out.println("몸무게(kg)를 입력해 주세요. : ");
+        double weight = sc.nextDouble();
+
+        double bmi = weight/(height * height);
+
+        System.out.println("BMI 지수 : " + bmi);
+
+        if(bmi < 18.5) {
+            System.out.println("저체중");
+        }else if(bmi < 23) {
+            System.out.println("정상체중");
+        }else if(bmi < 25) {
+            System.out.println("과체중");
+        }else if(bmi < 30) {
+            System.out.println("비만");
+        }else {
+            System.out.println("고도비만");
+        }
+
+    }
+
+    public void practice8() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("피연산자 1 입력 : ");
+        int num1 = sc.nextInt();
+        System.out.println("피연산자 2 입력 : ");
+        int num2 = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("연산자를 입력(+,-*,/,%) : ");
+        char ch = sc.nextLine().charAt(0);
+
+        if (num1 > 0 && num2 > 0) {
+            if (ch == '+') {
+                System.out.printf("%d %c %d = %d", num1, ch, num2, (num1 + num2));
+            } else if (ch == '-') {
+                System.out.printf("%d %c %d = %d ", num1, ch, num2, (num1 - num2));
+            } else if (ch == '*') {
+                System.out.printf("%d %c %d = %d", num1, ch, num2, (num1 * num2));
+            } else if (ch == '/') {
+                System.out.printf("%d %c %d = %d", num1, ch, num2, (num1 / num2));
+            } else if (ch == '%') {
+                System.out.printf("%d %c %d = %d", num1, ch, num2, (num1 % num2));
+            }
+        } else {
+            System.out.println("잘못 입력하셨습니다. 프로그램을 종료합니다.");
+
+        }
+
+    }
+
+    public void practice9() {
 
 
     }
---
+++ seokbong-java-workspace/05_control/src/main/java/com/jjanggu/secton02/loop/controller/Application.java
@@ -0,0 +1,45 @@
+package com.jjanggu.secton02.loop.controller;
+import com.jjanggu.secton02.loop.run.For;
+import com.jjanggu.secton02.loop.run.While;
+
+public class Application {
+
+    /*
+        ## 반복문 ##
+        1. 특정 문장들을 반복적으로 수행할 수 있도록 하는 구문
+        2. 종류
+           1) for문
+           2) while문
+              ㄴ do while문
+     */
+    public static void main(String[] args) {
+        While w = new While();
+//        w.testBasicWhile();
+//        w.testWhileExample1();
+//        w.testWhileExample2();
+//        w.testBasicDoWhile();
+//        w.testWhileExample2();
+
+        For f = new For();
+//        f.testBasicFor();
+//        f.testForExample1();
+//        f.testForExample2();
+//        f.testForExample3();
+//        f.testForExample4();
+//        f.testForExample5();
+//        f.testForExample6();
+//        f.testForExample7();
+//        f.testForExample8();
+//        f.testForExample9();
+//        f.testNestedFor();
+//        f.testNestedForExample();
+//        f.testNestedForExample2();
+//        f.testNestedForExample3();
+//        f.testNestedForExample4();
+        f.testInfinityLoop();
+
+
+
+
+    }
+}
