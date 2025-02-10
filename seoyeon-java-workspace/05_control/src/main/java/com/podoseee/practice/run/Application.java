489fe42fc64cfd172c4b2b94415d3268205fb26f
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-02-10 23:02:27 +0900

    05_control practice

---
+++ seoyeon-java-workspace/05_control/src/main/java/com/podoseee/practice/run/Application.java
@@ -0,0 +1,23 @@
+package com.podoseee.practice.run;
+
+import com.podoseee.practice.controller.LoopPractice;
+
+public class Application {
+    public static void main(String[]args) {
+        LoopPractice l = new LoopPractice();
+        //l.practice1();
+        //l.practice2();
+        //l.practice3();
+        //l.practice4();
+        //l.practice5();
+        //l.practice6();
+        //l.practice7();
+        //l.practice8();
+        //l.practice9();
+        //l.practice10();
+        //l.practice11();
+        //l.practice12();
+        //l.practice13();
+        l.practice14();
+    }
+}
