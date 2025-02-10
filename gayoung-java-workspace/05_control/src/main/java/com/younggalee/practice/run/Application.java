5fda60ce04ce4c98eb64ee9d1ec329554a2b61bd
이가영 <43063980+YoungGaLee@users.noreply.github.com>
2025-02-10 19:56:27 +0900

    Add LoopPractice file

--- gayoung-java-workspace/05_control/src/main/java/com/younggalee/practice/run/Application.java
+++ gayoung-java-workspace/05_control/src/main/java/com/younggalee/practice/run/Application.java
@@ -1,10 +1,29 @@
 package com.younggalee.practice.run;
 
 import com.younggalee.practice.controller.ConditionPractice;
+import com.younggalee.practice.controller.LoopPractice;
 
 public class Application {
     public static void main(String[] args) {
         ConditionPractice play = new ConditionPractice();
-        play.practice10();
+        LoopPractice loop = new LoopPractice();
+//        play.practice10();
+//        loop.practice1();
+//        loop.practice2();
+//        loop.practice3();
+//        loop.practice4();
+//        loop.practice5();
+//        loop.practice6();
+//        loop.practice7();
+//        loop.practice8();
+//        loop.practice9();
+//        loop.practice10();
+//        loop.practice11();
+//        loop.practice12();
+//        loop.practice13();
+        loop.practice14();
+
+
+
     }
 }
