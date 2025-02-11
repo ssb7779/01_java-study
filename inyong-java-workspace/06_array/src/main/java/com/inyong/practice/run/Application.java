d1858b1b4f45196d3cf8210f3374583690184e4b
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-11 17:30:57 +0900

    06_array

---
+++ inyong-java-workspace/06_array/src/main/java/com/inyong/practice/run/Application.java
@@ -0,0 +1,12 @@
+package com.inyong.practice.run;
+
+import com.inyong.practice.controller.ArrayPractice;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayPractice ap = new ArrayPractice();
+        ap.practice11();
+
+
+    }
+}
