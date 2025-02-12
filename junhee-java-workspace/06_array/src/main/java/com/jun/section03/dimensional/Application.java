35578229ef5396a838fd43d79ff636808d199f68
hyunjunhee <hojj4714@gmail.com>
2025-02-13 00:41:10 +0900

    06_array 최종

--- junhee-java-workspace/06_array/src/main/java/com/jun/section03/dimensional/Application.java
+++ junhee-java-workspace/06_array/src/main/java/com/jun/section03/dimensional/Application.java
@@ -3,11 +3,13 @@ package com.jun.section03.dimensional;
 public class Application {
     public static void main(String[] args) {
         DimensionalArray da = new DimensionalArray();
-        da.testDimensionalArray1();
+//        da.testDimensionalArray1();
+//        da.testDimensionalArray2();
+//        da.testDimensionalArray3();
+//        da.testDimensionalArray4();
+        da.testDimensionalArray5();
+
+
     }
-
-
-
-
-    }
+}
 
