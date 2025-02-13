42ebd1bdb1173b13d4ff950aed0fdacd43861e87
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-02-13 17:40:24 +0900

    <update> homework 내가한거 <author> podoseee

--- seoyeon-java-workspace/Homework_variable-to-control/src/main/java/com/podoseee/homework/run/Application.java
+++ seoyeon-java-workspace/Homework_variable-to-control/src/main/java/com/podoseee/homework/run/Application.java
@@ -1,13 +1,10 @@
 ﻿package com.podoseee.homework.run;
 
+import com.podoseee.homework.controller.Function;
 import com.podoseee.homework.view.Menu;
 
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        Menu menu = new Menu();
 
-        menu.displayMenu();
-    }
 }
