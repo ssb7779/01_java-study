8c07a635aca43e22f803a28a3ea4e729ac5bf771
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-02-14 17:51:21 +0900

    Update Homework_variable-to-control

--- seoyeon-java-workspace/Homework_variable-to-control/src/main/java/com/podoseee/homework/controller/Function.java
+++ seoyeon-java-workspace/Homework_variable-to-control/src/main/java/com/podoseee/homework/controller/Function.java
@@ -1,42 +1,51 @@
-﻿package com.podoseee.homework.controller;
+package com.podoseee.homework.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Function {
-        public void calculator(){
-                Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+    //1.
+    public void calculator(){
+        System.out.print("첫 번째 정수 : ");
+        int a = sc.nextInt();
 
-                System.out.print("첫 번째 정수 : ");
-                int a = sc.nextInt();
-                System.out.print("두 번째 정수 : ");
-                int b = sc.nextInt();
-                System.out.print("연산자(+, -, x, /) : ");
-                char op = sc.next().charAt(0);
-                int result = 0;
+        System.out.print("두 번째 정수 : ");
+        int b = sc.nextInt();
 
-                switch(op){
-                        case '+': result = a+b;
-                                break;
-                        case '-': result = a-b;
-                                break;
-                        case '*': result = a*b;
-                                break;
-                        case '/': result = a/b;
-                        if(b==0){
+        System.out.print("연산자(+, -, x, /) : ");
+        char op = sc.next().charAt(0);
+        int result = 0;
+
+        if(op == '/' && b == 0){
+            System.out.println("0으로 나눌 수 없습니다.");
+        }else {
+            switch (op) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                        /*if(b==0){
                                 System.out.print("0으로 나눌 수 없습니다.");
-                        }
-                                break;
-                        default :
-                                return;
-                }
-
-                System.out.printf("%d %c %d = %d", a, b, op, result);
+                        }*/ // if문 여기에 쓰지 말고 빼자.
+                    break;
+                default:
+                    System.out.println("존재하지 않는 연산자입니다.");
+                    return;
+            }
         }
 
-        public void totalCalculator(){
+        System.out.printf("%d %c %d = %d", a, b, op, result);
+    }
+
+    //2.
+        /*public void totalCalculator(){
                 Scanner sc = new Scanner(System.in);
 
                 System.out.print("첫 번째 정수 : ");
@@ -44,5 +53,5 @@ public class Function {
                 System.out.print("두 번째 정수 : ");
                 int b = sc.nextInt();
 
-        }
+        }*/
 }
