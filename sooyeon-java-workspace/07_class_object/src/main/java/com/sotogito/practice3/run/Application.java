c413a761e9c75c1984e7bfdf710c71edf56536cc
sotogito <sotogitoarchive@gmail.com>
2025-02-14 17:55:08 +0900

    feat : 객체 실습문제3(메서드연습) Function 클래스 작성

---
+++ sooyeon-java-workspace/07_class_object/src/main/java/com/sotogito/practice3/run/Application.java
@@ -0,0 +1,10 @@
+package com.sotogito.practice3.run;
+
+import com.sotogito.practice3.controller.Function;
+
+public class Application {
+    public static void main(String[] args) {
+        Function function = new Function();
+        System.out.println(function.alphabette());
+    }
+}
