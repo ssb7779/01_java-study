b31c75595620d8f0ab065bc7d31f3c5e82e34a5a
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-16 17:15:22 +0900

    Homework_variable-to-control

--- inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/controller/Function.java
+++ inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/controller/Function.java
@@ -206,40 +206,4 @@ public class Function {
 
         }
     }
-
-    public void displayMenu() {
-        System.out.println("1. 간단 계산기");
-        System.out.println("2. 작은 수에서 큰 수까지 합계");
-        System.out.println("3. 신상 정보 확인");
-        System.out.println("4. 학생 정보 확인");
-        System.out.println("5. 별과 숫자 출력");
-        System.out.println("6. 난수까지의 합계");
-        System.out.println("7. 구구단");
-        System.out.println("8. 주사위 숫자 알아맞추기 게임");
-        System.out.println("9. 프로그램 종료");
-        int num = 0;
-
-        while (true) {
-            System.out.println("메뉴 번호 : ");
-            num = sc.nextInt();
-            if (num > 10 && num < 1) {
-                System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
-            }
-            break;
-        }
-
-        switch (num){
-            case 1 : calculator();
-            case 2 : totalCalculator();
-            case 3 : printProfile();
-            case 4 : printScore();
-            case 5 : printStarNumber();
-            case 6 : sumRandom();
-            case 7 : exceptGugu();
-            case 8 : diceGame();
-            case 9 :
-                System.out.println("종료합니다.");
-                return;
-        }
-    }
 }
--- inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/run/Application.java
+++ inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/run/Application.java
@@ -1,11 +1,13 @@
 package com.inyong.homework.run;
 
 import com.inyong.homework.controller.Function;
+import com.inyong.homework.view.Menu;
 
 public class Application {
     public static void main(String[] args) {
         Function function = new Function();
+        Menu menu = new Menu();
 
-        function.displayMenu();
+        menu.displayMenu();
     }
 }
