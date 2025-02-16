33bc863d04575f249b8c4b09c5d86438c2744f96
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-16 17:07:01 +0900

    Homework_variable-to-control

--- inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/run/Application.java
+++ inyong-java-workspace/Homework_variable-to-control/src/main/java/com/inyong/homework/run/Application.java
@@ -1,7 +1,11 @@
 package com.inyong.homework.run;
 
+import com.inyong.homework.controller.Function;
+
 public class Application {
     public static void main(String[] args) {
+        Function function = new Function();
 
+        function.displayMenu();
     }
 }
