edbd4c9aaead1bd218d9fedde1ca9b3e0ddd9089
hyunjunhee <hojj4714@gmail.com>
2025-02-17 17:53:28 +0900

    07_class-object practice1~6

---
+++ junhee-java-workspace/07_class-object/src/main/java/com/jun/practice5/run/Application.java
@@ -0,0 +1,19 @@
+package com.jun.practice5.run;
+
+import com.jun.practice5.dto.Employee;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Employee[] emp = new Employee[3];
+        emp[0] = new Employee();
+        emp[1] = new Employee();
+        emp[2] = new Employee();
+
+
+        }
+        }
+
+
+
+
