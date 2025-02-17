8dcd1f4a15e596600715cf8b2ce8d7f33f6e55d3
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-17 17:55:35 +0900

    feat : practice 5 객체 배열 실습문제 추가 author : Kyungbae_Go
    -add practice5

---
+++ kyungbae-java-workspace/07_class-object/src/main/java/com/kyungbae/practice5/controller/Function.java
@@ -0,0 +1,16 @@
+package com.kyungbae.practice5.controller;
+
+import com.kyungbae.practice5.dto.Employee;
+
+public class Function {
+
+    public void deepDeepCopy(Employee[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+
+            }
+
+        }
+    }
+
+} // class end
