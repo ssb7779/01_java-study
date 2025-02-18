f453604ef0e9525c91597b72dc82f5a6e99d2d52
sotogito <sotogitoarchive@gmail.com>
2025-02-18 16:54:28 +0900

    feat : 상속 실습문제1 풀이

---
+++ sooyeon-java-workspace/08_inheritance/src/main/java/com/sotogito/practice1/dto/Student.java
@@ -0,0 +1,42 @@
+package com.sotogito.practice1.dto;
+
+public class Student extends Person {
+    private int grade;
+    private String major;
+
+    public Student() {
+    }
+
+    public Student(int age, double height, double weight,
+                   int grade, String major) {
+        super(age, height, weight);
+        this.grade = grade;
+        this.major = major;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                super.toString() +
+                "grade=" + grade +
+                ", major='" + major + '\'' +
+                '}';
+    }
+    
+}
