3353ed650838d8cf67d899adf1b7328478eb4c18
seokbong <ssb7779@naver.com>
2025-02-18 17:55:39 +0900

    08_inheritance_section4

---
+++ seokbong-java-workspace/08_inheritance/src/main/java/com/jjanggu/section04/overriding/SuperClass.java
@@ -0,0 +1,12 @@
+package com.jjanggu.section04.overriding;
+
+public class SuperClass {
+
+    public void method(int num){}
+
+    protected void protectedMethod() {}
+
+    private void privateMethod() {}
+
+    public final void finalMethod() {}
+}
