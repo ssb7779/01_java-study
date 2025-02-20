783c84bb9c18251e623e095535b2c6f35ca5e301
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-02-20 12:16:57 +0900

    09_polymorphism section03

---
+++ seoyeon-java-workspace/09_polymorphism/src/main/java/com/podoseee/section03/example2/Flyable.java
@@ -0,0 +1,7 @@
+package com.podoseee.section03.example2;
+
+public interface Flyable {
+
+    void fly();
+
+}
---
+++ seoyeon-java-workspace/09_polymorphism/src/main/java/com/podoseee/section03/example4/AmericanFood.java
@@ -0,0 +1,4 @@
+package com.podoseee.section03.example4;
+
+public class AmericanFood extends Food implements Eatable {
+}
