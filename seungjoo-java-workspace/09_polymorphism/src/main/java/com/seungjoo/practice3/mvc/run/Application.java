cff9d67af39d5ebdc5fed9df83ddf1709245971c
이승주 <eun07213@gmail.com>
2025-02-20 18:40:54 +0900

    0220 실습문제

---
+++ seungjoo-java-workspace/09_polymorphism/src/main/java/com/seungjoo/practice3/mvc/run/Application.java
@@ -0,0 +1,10 @@
+package com.seungjoo.practice3.mvc.run;
+
+import com.seungjoo.practice3.mvc.view.LibraryMenu;
+
+public class Application {
+    public static void main(String[] args) {
+        LibraryMenu menu = new LibraryMenu();
+        menu.mainMenu();
+    }
+}
