5922f320b0f7cf9fece0f732400d3d144cb23c9b
pch8349 <chan449@naver.com>
2025-02-20 17:36:57 +0900

    02-20 수업/실습내용

---
+++ pch-java-workspace/09_polymorphism/src/main/java/com/pch/section03/interface_implements/Behavior.java
@@ -0,0 +1,14 @@
+package com.pch.section03.interface_implements;
+
+public interface Behavior {
+
+    /*
+        ## 인터페이스 ##
+        1. interface
+        2. 추상메소드와 상수 필드만 가질 수 있는 클래스의 변형체
+
+     */
+
+    void behavior();
+    void work();
+}
