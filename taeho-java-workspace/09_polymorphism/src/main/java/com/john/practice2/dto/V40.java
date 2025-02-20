68a7e557d17c9aac0d5344d30674c017b5c8176e
TH <ok821000@gmail.com>
2025-02-20 18:34:41 +0900

    [Add] 다형성 실습문제 2번 풀이 진행

---
+++ taeho-java-workspace/09_polymorphism/src/main/java/com/john/practice2/dto/V40.java
@@ -0,0 +1,37 @@
+package com.john.practice2.dto;
+
+public class V40 extends SmartPhone {
+    public V40() {
+    }
+
+    @Override
+    public void makeACall() {
+        System.out.println("번호를 누르고 통화버튼을 누름");
+    }
+
+    @Override
+    public void takeACall() {
+        System.out.println("전화받기 버튼을 누름");
+    }
+
+    @Override
+    public void touch() {
+        System.out.println("정전식");
+    }
+
+    @Override
+    public void charge() {
+        System.out.println("고속충전");
+    }
+
+    @Override
+    public void picture() {
+        System.out.println("1200, 1600만 화소 트리플 카메라");
+    }
+
+
+    @Override
+    public void printMaker() {
+        System.out.println("V40는 LG에서 만들어 졌다.");
+    }
+}
