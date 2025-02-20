054a837b7fcc8a882dfe00fc6256d26772744057
sotogito <sotogitoarchive@gmail.com>
2025-02-20 17:46:13 +0900

    feat : 다형성 실습문제3(난이도상상상) DTO 작성

---
+++ sooyeon-java-workspace/09_polymorphism/src/main/java/practice3/mvc/dto/Book.java
@@ -0,0 +1,26 @@
+package practice3.mvc.dto;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+
+}
