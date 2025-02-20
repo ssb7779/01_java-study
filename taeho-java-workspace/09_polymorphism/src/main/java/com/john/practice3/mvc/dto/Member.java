6662bac85d6bbdaaf6ebebf2525b3606ee2002a7
TH <ok821000@gmail.com>
2025-02-20 19:52:27 +0900

    [Add] 다형성 실습문제 3 임시 저장

---
+++ taeho-java-workspace/09_polymorphism/src/main/java/com/john/practice3/mvc/dto/Member.java
@@ -0,0 +1,27 @@
+package com.john.practice3.mvc.dto;
+
+public class Member {
+    private String name;
+    private int age;
+    private char gender;
+    private int couponCount = 0;
+
+    public Member() {
+    }
+
+    public Member(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", couponCount=" + couponCount +
+                '}';
+    }
+}
