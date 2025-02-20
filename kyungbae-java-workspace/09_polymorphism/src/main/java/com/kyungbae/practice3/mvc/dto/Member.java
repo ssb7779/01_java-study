8688eec30c600ffac48094b30e0bc266edb3d3b4
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-21 00:58:15 +0900

    update : practice3 mvc author : Kyungbae_Go
    다형성 실습문제 3 해결
    
    -add controller
    -add dto
    -add run
    -add view

--- kyungbae-java-workspace/09_polymorphism/src/main/java/com/kyungbae/practice3/mvc/dto/Member.java
+++ kyungbae-java-workspace/09_polymorphism/src/main/java/com/kyungbae/practice3/mvc/dto/Member.java
@@ -9,11 +9,10 @@ public class Member {
 
     public Member(){}
 
-    public Member(String name, int age, char gender, int couponCount) {
+    public Member(String name, int age, char gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
-        this.couponCount = couponCount;
     }
 
     @Override
@@ -24,5 +23,37 @@ public class Member {
                 ", gender=" + gender +
                 ", couponCount=" + couponCount +
                 '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getCouponCount() {
+        return couponCount;
+    }
+
+    public void setCouponCount(int couponCount) {
+        this.couponCount = couponCount;
     }
 }
