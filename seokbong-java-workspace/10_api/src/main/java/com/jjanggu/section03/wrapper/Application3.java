5095ba67d746ddd15b1d864a2b17e66bd6d5c468
seokbong <ssb7779@naver.com>
2025-02-21 17:50:56 +0900

    10_api section 03

---
+++ seokbong-java-workspace/10_api/src/main/java/com/jjanggu/section03/wrapper/Application3.java
@@ -0,0 +1,21 @@
+package com.jjanggu.section03.wrapper;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+    }
+    /*
+        ## 기본자료형 => 문자열 ##
+        기본자료형을 문자열로 변경
+     */
+
+
+    // 1) Wrapper 클래스 사용 : valueOf 메소드를 통해 객체화 시키고 toStirng 통해 문자열화
+    String i = Integer.valueOf(4).toString(); // "4"
+
+    // 2) String 클래스의 static valueOf 메소드 사용
+    String str = String.valueOf(10); // "10"
+
+    // 3) 문자열 합치기 사용
+    String str2 = 123+ ""; // "123"
+}
