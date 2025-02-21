42db9dc7599b8383225ed0c3cec59b9ab5ef146b
inhomun <inho8063@naver.com>
2025-02-21 17:02:32 +0900

    docs_complete_250221

---
+++ ino-java-workspace/10_api/src/main/java/com/ino/section03/wrapper/Application3.java
@@ -0,0 +1,12 @@
+package com.ino.section03.wrapper;
+
+public class Application3 {
+    public static void main(String[] args) {
+        // primitive -> String
+
+        //1) using Wrapper class
+        String i = Integer.valueOf(12324114).toString();
+
+        String s = String.valueOf(12321421);
+    }
+}
