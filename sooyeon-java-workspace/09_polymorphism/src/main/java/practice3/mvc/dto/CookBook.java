a7cebae39d22b97bc452085268073c5476ad9e8a
sotogito <sotogitoarchive@gmail.com>
2025-02-21 18:50:11 +0900

    feat : 다형성 실습문제3(난이도상상상) 풀이

--- sooyeon-java-workspace/09_polymorphism/src/main/java/practice3/mvc/dto/CookBook.java
+++ sooyeon-java-workspace/09_polymorphism/src/main/java/practice3/mvc/dto/CookBook.java
@@ -13,19 +13,20 @@ public class CookBook extends Book {
 
     /**
      * 쿠폰을 내부에서 직접 정용하게? 아니면 유무확인ㅁ나하고 ?외부에?
+     *
      * @return
      */
 
-    public void updateUserCoupon(Member member){
-        if(coupon){
+    public void updateUserCoupon(Member member) {
+        if (coupon) {
             member.updateCouponCount(1);
         }
     }
 
-
     @Override
     public String toString() {
         return "CookBook{" +
+                super.toString() +
                 "coupon=" + coupon +
                 '}';
     }
