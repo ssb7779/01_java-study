599fbd88e810d18e923cc34a1fdfa576de9f4ffa
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-24 17:17:19 +0900

    feat : MVC 예제 MusicProgram 을 예로 author : Kyungbae_Go
    MusicProgram 을 작동하기 위한
    Model(dto), View, Controller 예제
    
    -add controller
    -add dto
    -add run
    -add view

---
+++ kyungbae-java-workspace/11_collection/src/main/java/com/kyungbae/section02/list/dto/Music.java
@@ -0,0 +1,59 @@
+package com.kyungbae.section02.list.dto;
+
+public class Music implements Comparable<Music> {
+
+    private String title;
+    private String artist;
+
+    public Music(){}
+
+    public Music(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Music{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Music o) {
+        // Collections.sort 메소드 수행시 내부적으로 Music객체간에 비교할 때 호출되는 메소드
+        // 즉, mi.compareTo(m2) 와 같이 호출됨
+        // 이때 양수가 반환될 경우 두 객체의 순서가 변경되게 되어있음
+
+        // ex) 가수명으로 오름차순 정렬 하도록
+        //      => 앞의 Music 가수명이 크고, 뒤의 Music 가수명이 작을경우
+        //      => 순서가 바뀔수 있도록 (양수 반환)
+        /*
+        if(this.artist > o.artist){
+            return 양수;
+        }else{
+            return 음수;
+        }
+         */
+
+        return this.artist.compareTo(o.artist); // 문자열의 compareTo : 앞의 문자열이 더 클경우 양수 반환
+
+    }
+}
