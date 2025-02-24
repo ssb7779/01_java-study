f80638713a0a01deda09bf9f5f171071ea643b55
ibe6 <nxnxnxnx626@gmail.com>
2025-02-24 17:21:22 +0900

    Update MusicView.java

--- JungYeon-java-workspace/11_collection/src/main/java/com/ibe6/section02/list/view/MusicView.java
+++ JungYeon-java-workspace/11_collection/src/main/java/com/ibe6/section02/list/view/MusicView.java
@@ -14,6 +14,8 @@ public class MusicView {
     // 메인화면
     public void mainMenu() {
 
+        System.out.println("학원에서 작업한 코드");
+
         while (true) {
             System.out.println("\n=== 음악 관리 프로그램 ===");
             System.out.println("1. 신규 곡 추가");
