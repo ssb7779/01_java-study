cad5e9ca3b63d1563814e780a3c21c5be0e1615f
seokbong <ssb7779@naver.com>
2025-02-25 18:05:03 +0900

    11_collection section4, practice

---
+++ seokbong-java-workspace/11_collection/src/main/java/com/jjanggu/practice1/mvc/controller/BookController.java
@@ -0,0 +1,43 @@
+package com.jjanggu.practice1.mvc.controller;
+
+import com.jjanggu.practice1.mvc.dto.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookController {
+
+    private List<Book> bookList = new ArrayList<>();
+    {
+     bookList.add(new Book("자바언어의 정석", "홍길동", "기타", 20000));
+     bookList.add(new Book("C언어의 보람", "김말똥", "기타", 15000));
+     bookList.add(new Book("도움이 되었나요", "캥거루", "인문", 17500));
+     bookList.add(new Book("여러분", "홍홍홍", "의료", 10000));
+    }
+
+    public boolean insertBook(String title, String author, int category, String price){
+        String categoryName = "";
+        switch(category){
+            case 1: categoryName = "인문"; break;
+            case 2: categoryName = "자연과학"; break;
+            case 3: categoryName = "의료"; break;
+            case 4: categoryName = "기타"; break;
+            default:
+                System.out.println("잘못된 번호입니다.");
+                return false;
+        }
+        int newPrice = Integer.parseInt(price) ; // string  => int
+
+        boolean result = bookList.add(new Book(title, author, categoryName, newPrice));
+        return result;
+    }
+
+    public List<Book> selectList(){
+        return bookList;
+    }
+
+    public void searchBook(){
+
+    }
+
+}
---
+++ seokbong-java-workspace/11_collection/src/main/java/com/jjanggu/practice1/mvc/view/BookMenu.java
@@ -0,0 +1,96 @@
+package com.jjanggu.practice1.mvc.view;
+
+import com.jjanggu.practice1.mvc.controller.BookController;
+import com.jjanggu.practice1.mvc.dto.Book;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BookMenu {
+
+    private Scanner sc = new Scanner(System.in);
+    private BookController bc = new BookController();
+
+
+    public void mainMenu(){
+
+        while(true){
+            System.out.println("\n===== 메인 메뉴 =====");
+            System.out.println("1. 새 도서 추가");
+            System.out.println("2. 도서 전체 조회");
+            System.out.println("3. 도서 검색 조회");
+            System.out.println("4. 도서 삭제하기");
+            System.out.println("0. 프로그램 종료하기");
+            System.out.println(">> 메뉴 번호 선택");
+            int menu = sc.nextInt();
+            sc.nextLine();
+
+            switch(menu){
+                case 1: insertBook(); break;
+                case 2: selectList(); break;
+                case 3: searchBook(); break;
+                case 4:break;
+                case 0:
+                    System.out.println("프로그램 종료");
+                    return;
+                default:
+                    System.out.println("잘못입력하셨습니다. 다시 선택해주세요");
+            }
+        }
+
+
+    }
+
+    public void insertBook(){
+        System.out.println("도서명 : ");
+        String title = sc.nextLine();
+        System.out.println("저자명 : ");
+        String author = sc.nextLine();
+        System.out.println();
+        System.out.println("장르번호 (1:인문 / 2:자연과학 / 3:의료 / 4:기타) : ");
+        int category = sc.nextInt();
+        sc.nextLine();
+        System.out.println("가격 : ");
+        String price = sc.nextLine();
+
+        boolean result = bc.insertBook(title, author, category, price);
+        if (result){
+            System.out.println("성공적으로 추가되었습니다.");
+        }else{
+            System.out.println("추가에 실패하였습니다.");
+        }
+
+
+    }
+
+    public void selectList() {
+        List<Book> list = bc.selectList();
+
+        if(list.isEmpty()){
+            System.out.println("존재하는 도서가 없습니다.");
+        }else{
+            for(int i = 0; i < list.size(); i++){
+                System.out.println(list.get(i));
+            }
+        }
+
+    }
+
+    public void searchBook() {
+        System.out.println("===== 검색 메뉴=====");
+        System.out.println("1. 도서명으로 검색");
+        System.out.println("2. 저자명으로 검색");
+        System.out.println("3. 도서명+저자명으로 검색");
+        System.out.println("0. 뒤로가기");
+        System.out.println(">> 메뉴 번호 선택 : ");
+        int menu = sc.nextInt();
+        sc.nextLine();
+
+
+
+    }
+
+    public void deleteBook() {
+
+    }
+}
