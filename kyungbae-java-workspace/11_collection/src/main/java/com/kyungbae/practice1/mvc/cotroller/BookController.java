435d0a0b68175d3f1f7198e3f23fc1fe52b72a17
Kyungbae_Go <gokyungbae@gmail.com>
2025-02-25 17:52:27 +0900

    feat : practice1 풀이중 author : Kyungbae_Go
    컬렉션 실습문제1
    
    -add practice1

---
+++ kyungbae-java-workspace/11_collection/src/main/java/com/kyungbae/practice1/mvc/cotroller/BookController.java
@@ -0,0 +1,38 @@
+package com.kyungbae.practice1.mvc.cotroller;
+
+import com.kyungbae.practice1.mvc.dto.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookController {
+
+    private List<Book> bookList = new ArrayList<>();
+    {    // 초기값 4개정도 추가
+        bookList.add(new Book("자바언어의 정석", "홍길동", "기타", 20000));
+        bookList.add(new Book("C언어의 보람", "김말똥", "기타", 15000));
+        bookList.add(new Book("도움이 되었나요", "캥거루", "인문", 17500));
+        bookList.add(new Book("여러분", "홍홍홍", "의료", 10000));
+    }
+
+    public int insertBook(String title, String author, int category, String price){
+        int work = 0;
+        int intPrice = Integer.parseInt(price);
+        String strCategory = null;
+        switch (category) {
+            case 1 : strCategory = "인문"; work++; break;
+            case 2 : strCategory = "자연과학"; work++; break;
+            case 3 : strCategory = "의료"; work++; break;
+            case 4 : strCategory = "기타"; work++; break;
+        }
+        bookList.add(new Book(title, author, strCategory, intPrice));
+
+        return work;
+    }
+
+    public List<Book> selectList() {
+        return bookList;
+    }
+
+
+}
---
+++ kyungbae-java-workspace/11_collection/src/main/java/com/kyungbae/practice1/mvc/run/Application.java
@@ -0,0 +1,9 @@
+package com.kyungbae.practice1.mvc.run;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+
+    }
+}
