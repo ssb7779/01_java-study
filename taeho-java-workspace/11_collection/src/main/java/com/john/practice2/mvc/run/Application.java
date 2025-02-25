171a24fe1b5fe48ed321519160e724aa0aaf934c
TH <ok821000@gmail.com>
2025-02-25 21:20:02 +0900

    [Add] 컬렉션 실습문제 2번 풀이

---
+++ taeho-java-workspace/11_collection/src/main/java/com/john/practice2/mvc/run/Application.java
@@ -0,0 +1,10 @@
+package com.john.practice2.mvc.run;
+
+import com.john.practice2.mvc.view.LibraryMenu;
+
+public class Application {
+    public static void main(String[] args) {
+        LibraryMenu lm = new LibraryMenu();
+        lm.mainMenu();
+    }
+}
