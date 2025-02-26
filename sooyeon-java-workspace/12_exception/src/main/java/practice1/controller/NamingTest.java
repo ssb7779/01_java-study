99f7f82327a6ebd8c8eb61536f87531f2d7371e8
sotogito <sotogitoarchive@gmail.com>
2025-02-26 17:11:28 +0900

    feat : 예외처리 실습문제1 풀이

---
+++ sooyeon-java-workspace/12_exception/src/main/java/practice1/controller/NamingTest.java
@@ -0,0 +1,65 @@
+package practice1.controller;
+
+import practice1.exception.NamingException;
+
+import java.util.regex.Pattern;
+
+public class NamingTest {
+    private static String[] reservedWord = {"abstract", "assert", "boolean", "break", "byte", "case"
+            , "catch", "char", "class", "const", "continue", "default"
+            , "do", "double", "else", "enum", "extends", "false", "final"
+            , "finally", "float", "for", "if", "goto", "implements"
+            , "import", "instanceof", "int", "interface", "long", "native"
+            , "new", "null", "package", "private", "protected", "public"
+            , "return", "short", "static", "strictfp", "super", "switch"
+            , "synchronized", "this", "throw", "throws", "transient", "true"
+            , "try", "void", "volatile", "while"};
+
+
+    public NamingTest() {
+    }
+
+    public boolean isReservedWord(String user) {
+        user = user.toLowerCase();
+        for (String res : reservedWord) {
+            if (res.equals(user)) {
+                throw new NamingException("변수 명명 규칙에 어긋납니다.(예약어사용불가능)");
+            }
+        }
+        return true;
+    }
+
+    public boolean isSpecialWord(String user) {
+        for (String strToken : user.split("")) {
+            char token = strToken.charAt(0);
+            /// 특수문자라면
+            if (token == '$' || token == '_' && Character.isLetterOrDigit(token)) {
+                continue;
+            }
+            /*
+            /// 숫자라면
+            if (Character.isDigit(token)) {
+                continue;
+            }
+            /// 문자라면
+            if (Character.isUpperCase(token) || Character.isLowerCase(token)) {
+                continue;
+            }
+             */
+            throw new NamingException("변수 명명규칙에 어긋납니다.(영문자와 ‘_’, ‘$’, 숫자 사용가능)");
+        }
+//        /// 대+소문자 조합이 아닌경우 예외 - 없어됨 소문자만일수ㅇㅋ
+//        if (!isContainUpperCase || !isContainLowerCase) {
+//            throw new NamingException("변수 명명규칙에 어긋납니다.(대소문자 모두 포함)");
+//        }
+        return true;
+    }
+
+    public boolean isNumFirst(String user) {
+        if (Character.isDigit(user.charAt(0))) {
+            throw new NamingException("변수 명명규칙에 어긋납니다.(맨 앞 숫자 불가능)");
+        }
+        return true;
+    }
+
+}
