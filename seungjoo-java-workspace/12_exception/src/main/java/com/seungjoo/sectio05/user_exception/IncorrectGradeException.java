e31b030d123d56603bc57f93ae132b028008d22d
이승주 <eun07213@gmail.com>
2025-02-26 16:45:25 +0900

    0226

---
+++ seungjoo-java-workspace/12_exception/src/main/java/com/seungjoo/sectio05/user_exception/IncorrectGradeException.java
@@ -0,0 +1,14 @@
+package com.seungjoo.sectio05.user_exception;
+
+public class IncorrectGradeException extends Exception{
+
+
+
+
+    public IncorrectGradeException() {}
+
+    public IncorrectGradeException(String message){
+        super(message);
+    }
+
+}
