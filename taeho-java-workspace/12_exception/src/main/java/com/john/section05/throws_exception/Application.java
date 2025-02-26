2ef0716433f352006ea4c2e7ceb49c50516a966a
TH <ok821000@gmail.com>
2025-02-26 16:08:52 +0900

    [Add] 예외처리에 대한 학습 진행

---
+++ taeho-java-workspace/12_exception/src/main/java/com/john/section05/throws_exception/Application.java
@@ -0,0 +1,27 @@
+package com.john.section05.throws_exception;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        ThrowTest tt = new ThrowTest();
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("점수 입력(0~100): ");
+            int score = sc.nextInt();
+            tt.checkScore(score);
+
+            System.out.print("나이 입력(0~120): ");
+            int age = sc.nextInt();
+            tt.checkAge(age);
+
+            System.out.println("등급 입력(A|B|C|D): ");
+            char grade = sc.next().charAt(0);
+            tt.checkGrade(grade);
+        }catch (Exception e) {
+            System.out.println(e.getClass().getName());
+            System.out.println(e.getMessage());
+        }
+    }
+}
