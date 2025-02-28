a5b25a1613b8a437090cf336095949897fcd457b
TH <ok821000@gmail.com>
2025-02-28 15:39:34 +0900

    [Add] 13강 입출력 학습

---
+++ taeho-java-workspace/13_io/src/main/java/com/john/section03/assist_stream/buffered/Application.java
@@ -0,0 +1,9 @@
+package com.john.section03.assist_stream.buffered;
+
+public class Application {
+    public static void main(String[] args) {
+        BufferedDao bd = new BufferedDao();
+//        bd.fileSave();
+        bd.fileCopy();
+    }
+}
---
+++ taeho-java-workspace/13_io/src/main/java/com/john/section03/assist_stream/object/Application.java
@@ -0,0 +1,10 @@
+package com.john.section03.assist_stream.object;
+
+public class Application {
+    public static void main(String[] args) {
+        ObjectDao objectDao = new ObjectDao();
+//        objectDao.fileSave();
+        objectDao.fileRead();
+    }
+
+}
