8cd25fe327bb39fe37d92aa6c9960c08afb190e0
이가영 <43063980+YoungGaLee@users.noreply.github.com>
2025-02-28 15:31:02 +0900

    In/Output Stream(Byte, file, Data, Object, Buffered) 수업코드

---
+++ gayoung-java-workspace/11_Collection/src/main/java/com/younggalee/section04/map/run/Application4.java
@@ -0,0 +1,28 @@
+package com.younggalee.section04.map.run;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Application4 {
+    public static void main(String[] args) {
+        Properties prop = new Properties();//텅 빈 상태
+
+        try {
+            // load(InputStream) : 파일에 기록된 key-value를 읽어들여서 Properties에 저장
+            prop.load(new FileInputStream("test.properties"));
+            // loadFromXML(InputStream) : xml을 읽어들일때 사용
+            prop.loadFromXML(new FileInputStream("test.xml"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        System.out.println(prop);
+
+        // getProperty(String key) : value를 String 타입으로 변환 (존재하지 않을 경우 null) : 오타주의
+        System.out.println(prop.getProperty("Set"));  // HashSet
+        System.out.println(prop.getProperty("set"));  // null
+
+    }
+}
