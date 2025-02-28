98dba64e91a8f6f8d4b1c00abe11fb7d4bdf53eb
sotogito <sotogitoarchive@gmail.com>
2025-02-28 15:45:49 +0900

    refactor : Comparator 오버라이드 어노테이션 추가

--- sooyeon-java-workspace/11_collection/src/main/java/practice2/mvc/comparator/BookAuthorComparator.java
+++ sooyeon-java-workspace/11_collection/src/main/java/practice2/mvc/comparator/BookAuthorComparator.java
@@ -5,7 +5,10 @@ import practice2.mvc.dto.Book;
 import java.util.Comparator;
 
 public class BookAuthorComparator implements Comparator<Book> {
+
+    @Override
     public int compare(Book o1, Book o2) {
         return o1.getAuthor().compareTo(o2.getAuthor());
     }
+
 }
--- sooyeon-java-workspace/11_collection/src/main/java/practice2/mvc/comparator/BookTitleComparator.java
+++ sooyeon-java-workspace/11_collection/src/main/java/practice2/mvc/comparator/BookTitleComparator.java
@@ -5,7 +5,10 @@ import practice2.mvc.dto.Book;
 import java.util.Comparator;
 
 public class BookTitleComparator implements Comparator<Book> {
+
+    @Override
     public int compare(Book o1, Book o2) {
         return o1.getTitle().compareTo(o2.getTitle());
     }
+
 }
