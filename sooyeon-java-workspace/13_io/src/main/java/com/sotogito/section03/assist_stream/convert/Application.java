471b9ad7e8eec7840baec7754cd33e32311cc6b4
sotogito <sotogitoarchive@gmail.com>
2025-02-28 12:42:04 +0900

    feat : 표준스트림

---
+++ sooyeon-java-workspace/13_io/src/main/java/com/sotogito/section03/assist_stream/convert/Application.java
@@ -0,0 +1,8 @@
+package com.sotogito.section03.assist_stream.convert;
+
+public class Application {
+    public static void main(String[] args) {
+        ByteToCharDao dao = new ByteToCharDao();
+        dao.consoleOutput();
+    }
+}
