930fd5f50e80b16a1054905447e098d29e733ed0
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-03-04 12:45:25 +0900

    14_lambda section02

---
+++ seoyeon-java-workspace/14_lambda/src/main/java/com/podoseee/section02/lambda/my_functional_interface/FuncInterface1.java
@@ -0,0 +1,11 @@
+package com.podoseee.section02.lambda.my_functional_interface;
+
+
+/*
+    ## 함수형 인터페이스 ##
+ */
+
+@FunctionalInterface
+public interface FuncInterface1 {
+    void print(); // 무언가를 출력하는 용도의 메소드
+}
