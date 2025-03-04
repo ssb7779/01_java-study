62194fd71d314962f39cfaf80643b488212f0f65
pch8349 <chan449@naver.com>
2025-03-04 17:52:45 +0900

    03-04 수업내용

---
+++ pch-java-workspace/14_lambda/src/main/java/com/pch/section01/interface_implements/SubCalculator.java
@@ -0,0 +1,7 @@
+package com.pch.section01.interface_implements;
+
+public class SubCalculator implements Calculator {
+    public int calc(int num1, int num2) {
+        return num1 - num2;
+    }
+}
