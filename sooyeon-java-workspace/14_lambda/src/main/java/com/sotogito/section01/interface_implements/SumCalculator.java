001f41aacf6f5bb44c52828b3c6eeb360a2c5e6b
sotogito <sotogitoarchive@gmail.com>
2025-03-04 22:29:06 +0900

    feat : Functional Interface 람다 적용

---
+++ sooyeon-java-workspace/14_lambda/src/main/java/com/sotogito/section01/interface_implements/SumCalculator.java
@@ -0,0 +1,8 @@
+package com.sotogito.section01.interface_implements;
+
+public class SumCalculator implements Calculator {
+    @Override
+    public int calc(int num1, int num2) {
+        return num1 + num2;
+    }
+}
