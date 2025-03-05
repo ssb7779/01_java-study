99f2754256a6b1e8030b5315d8b10a00f0b5416f
sotogito <sotogitoarchive@gmail.com>
2025-03-05 16:55:40 +0900

    feat : InetAddress

--- sooyeon-java-workspace/16_network/src/main/java/com/sotogito/section01/inetaddress/Application1.java
+++ sooyeon-java-workspace/16_network/src/main/java/com/sotogito/section01/inetaddress/Application1.java
@@ -1,5 +1,29 @@
 package com.sotogito.section01.inetaddress;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 public class Application1 {
-    
+    public static void main(String[] args) {
+        /**
+         * InetAddress
+         */
+
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+
+            System.out.println(address);
+            System.out.println(address.getHostAddress());
+            System.out.println(address.getHostName());
+
+            InetAddress[] addrList = InetAddress.getAllByName("www.naver.com");
+            Arrays.stream(addrList)
+                    .forEach(a -> System.out.println(a.getHostAddress()));
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
