e94d566984c6eff8314b9a25d334bb88b8cef029
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-03-05 17:04:19 +0900

    15_stream section03

---
+++ seoyeon-java-workspace/15_stream/src/main/java/com/podoseee/dto/Product.java
@@ -0,0 +1,47 @@
+package com.podoseee.dto;
+
+public class Product {
+
+    private String name;
+    private String category;
+    private int price;
+
+    public Product(String name, String category, int price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
